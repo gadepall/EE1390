3d0882d321718b2397ccde570a23253914d50974
gadepall <gadepall@gmail.com>
2019-02-13 17:29:46 +0530

    Add existing files

---
+++ manuals/supervised/linear_class/codes/2.3.py
@@ -0,0 +1,22 @@
+#Importing numpy and pyplot
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#Function for generating coin toss
+def coin(x):
+	return 2*np.random.randint(2,size=x)-1
+	
+
+simlen = int(1e5)
+N = np.random.normal(0,1,simlen)
+X = coin(simlen)
+A = 4
+Y = A*X+N
+
+plt.plot(Y,'o')
+plt.xlabel('$Sample}$')
+plt.ylabel('$Y$')
+plt.grid()
+plt.show()
+
