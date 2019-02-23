60fc507d6d4fe0b5ba7b5d747d9c1e3cafae0daf
gadepall <gadepall@gmail.com>
2019-02-23 08:47:17 +0530

    Add existing files

--- manuals/supervised/linear_class/codes/2.3.py
+++ manuals/supervised/linear_class/codes/2.3.py
@@ -1,6 +1,10 @@
 #Importing numpy and pyplot
 import numpy as np
 import matplotlib.pyplot as plt
+#If using termux
+import subprocess
+import shlex
+#end if
 
 
 #Function for generating coin toss
@@ -13,3 +17,14 @@ N = np.random.normal(0,1,simlen)
 S = coin(simlen)
 A = 4
 X = A*S+N
+plt.plot(X,'o')
+plt.xlabel('$Sample}$')
+plt.ylabel('$X$')
+plt.grid()
+#If using termux
+plt.savefig('../figs/bpsk.pdf')
+plt.savefig('../figs/bpsk.eps')
+subprocess.run(shlex.split("termux-open ../figs/bpsk.pdf"))
+#else
+#plt.show()
+
